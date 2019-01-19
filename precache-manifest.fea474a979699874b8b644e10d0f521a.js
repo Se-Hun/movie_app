5dd3f1fcf805e47986791133128f4f46f168e9bb
Se-Hun <tpgns0539@naver.com>
2019-01-19 03:40:00 -0800

    Updates

---
+++ precache-manifest.fea474a979699874b8b644e10d0f521a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f65cbfe0d681a00f6ba",
+    "url": "/movie_app/static/css/main.bfd7c0a9.chunk.css"
+  },
+  {
+    "revision": "8f65cbfe0d681a00f6ba",
+    "url": "/movie_app/static/js/main.8f65cbfe.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "6dc318a38153af4cc6070243ad2d87af",
+    "url": "/movie_app/index.html"
+  }
+];
